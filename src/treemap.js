4036f99c5c84fd11d12d3a88797d3b31a70dbb3c
Rodrigo Ramos <rodrigo@figurasthlm.se>
2021-06-16 00:34:40 +0200

    qwe

--- src/treemap.js
+++ src/treemap.js
@@ -297,12 +297,15 @@ const vis = {
         const getTooltip = function(d) {
             var tiptext = "";
             if (d.height === 0) {
+                let p = 0
                 for (var prop in hierarchy_names) {
                     var metadata = d.data.metadata[hierarchy_names[prop]];
-                    if(metadata.rendered != null){
-                        tiptext += " " + metadata.rendered + ""; //<p><em>" + metadata.label + ":</em> 
+                    if( p > 0){
+                        if(metadata.rendered != null){
+                            tiptext += " " + metadata.rendered + ""; //<p><em>" + metadata.label + ":</em> 
+                        }
                     }
-                    
+                    p++
                 }
                 tiptext += '<br>'
                 for (var measure in measures) {
