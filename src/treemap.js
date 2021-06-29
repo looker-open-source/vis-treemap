03fb3810f5da8d00f42271b599dcfac281e1a451
Rodrigo Ramos <rodrigo@figurasthlm.se>
2021-06-29 13:04:52 +0200

    qew

--- src/treemap.js
+++ src/treemap.js
@@ -154,7 +154,7 @@ const vis = {
         const chartCentreY = bounds.y + (bounds.height / 2);
 
         const headerColor = defaultHeaderColor;
-        const number_of_headers = 3;
+        const number_of_headers = 2;
 
         const dimensions = queryResponse.fields.dimension_like;
         const measures = queryResponse.fields.measure_like;
